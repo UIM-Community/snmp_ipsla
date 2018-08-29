043e1d32903185b941a4ddf1de5bfd7d7f71799a
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-08-29 02:07:34 +0200

    Improve XML field checking by adding empty string check

--- src/device.pm
+++ src/device.pm
@@ -3,19 +3,24 @@ package src::device;
 # Perl Core package(s)
 use strict;
 
+# Check if an XML field is defined or not
+sub isNotDefined {
+    return !defined @_[0] || ref @_[0] eq "HASH" || @_[0] eq "" ? 1 : 0;
+}
+
 # Device Prototype Constructor
 sub new {
     my ($class, $self) = @_;
-    my $ref = ref($self->{PrimaryIPV4Address});
-    if(!defined($self->{PrimaryIPV4Address}) || ref($self->{PrimaryIPV4Address}) eq "HASH") {
+    if(isNotDefined($self->{PrimaryIPV4Address})) {
         warn "Property 'PrimaryIPV4Address' is missing (from XML File) for device with UUID <$self->{ElementUUID}>\n";
     }
-    if(!defined($self->{SnmpProfileUUID}) || ref($self->{SnmpProfileUUID}) eq "HASH") {
+    if(isNotDefined($self->{SnmpProfileUUID})) {
         warn "Property 'SnmpProfileUUID' is missing (from XML File) for device with UUID <$self->{ElementUUID}>\n";
     }
-    if(!defined($self->{Label}) || ref($self->{Label}) eq "HASH") {
+    if(isNotDefined($self->{Label})) {
         warn "Property 'Label' is missing (from XML File) for device with UUID <$self->{ElementUUID}>\n";
     }
+
     return bless($self, ref($class) || $class)
 }
 
--- src/snmp.pm
+++ src/snmp.pm
@@ -3,10 +3,15 @@ package src::snmp;
 # Perl Core package(s)
 use strict;
 
+# Check if an XML field is defined or not
+sub isNotDefined {
+    return !defined @_[0] || ref @_[0] eq "HASH" || @_[0] eq "" ? 1 : 0;
+}
+
 # SNMP Prototype Constructor
 sub new {
     my ($class, $self) = @_;
-    if(!defined($self->{SnmpProfileUUID}) || ref($self->{SnmpProfileUUID}) eq "HASH") {
+    if(isNotDefined($self->{SnmpProfileUUID})) {
         warn "Property 'SnmpProfileUUID' is missing (from XML File) for one of the SnmpProfile\n";
     }
     return bless($self, ref($class) || $class)
