bcd63f52cab8ea2237f5e0c618da2242aca668d0
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-03 17:40:35 +0200

    Reverse metrics types

--- db/metrics_definition.sql
+++ db/metrics_definition.sql
@@ -1,10 +1,10 @@
 INSERT INTO cm_configuration_item_definition (ci_type,ci_parent,ci_description) VALUES ('9.1.2','9.1','Private.Device.Nokia');
 INSERT INTO cm_configuration_item_definition (ci_type,ci_parent,ci_description) VALUES ('9.1.2.1','9.1.2','Private.Device.Nokia.ResponsePathTest');
 
-INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2:0','Reachability','health','9.1.2');
-INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2:1','GetTableFail','health','9.1.2');
+INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2:0','Reachability','','9.1.2');
+INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2:1','GetTableFail','','9.1.2');
 
-INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2.1:0','TestRunResult','health','9.1.2.1');
+INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2.1:0','TestRunResult','','9.1.2.1');
 INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2.1:1','MinRttWithPrecision','µs','9.1.2.1');
 INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2.1:2','MaxRttWithPrecision','µs','9.1.2.1');
 INSERT INTO cm_configuration_item_metric_definition (met_type, met_description, unit_type, ci_type) VALUES ('9.1.2.1:3','AvgRttWithPrecision','µs','9.1.2.1');
